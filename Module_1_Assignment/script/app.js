0b8352d036eb1c4d938ad722d2cde8108363f3c7
AkeleA <achorisaac@gmail.com>
2020-11-14 12:40:52 +0100

    corrections

--- Module_1_Assignment/script/app.js
+++ Module_1_Assignment/script/app.js
@@ -1,47 +1,42 @@
 (function() {
   'use strict';
 
-  angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
 
-  LunchCheckController.$inject = ['$scope', '$filter'];
+  LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
       $scope.dishes = "";
       $scope.messages = "";
-      $scope.color1 = 'gray';
+      
 
-      $scope.check = function() {
-          if ($scope.dishes.length === 0) {
-              $scope.messages = "Please enter data first!";
-              $scope.color1 = 'red';
 
-          } else {
-              $scope.color1 = 'green';
-              var i = 0;
-              var nani = 0;
-              var nuDishes = $scope.dishes.split(',');
-              nuDishes.forEach((function(value) {
-                  if (value.trim().length > 0) {
-                      i++;
-                  } else {
-                      nani++;
-                  }
-              }
-              ));
-              if (i < 4) {
-                  $scope.messages = "Enjoy!";
-              } else {
-                  $scope.messages = "Too much!";
-              }
-          }
+      $scope.click = function () {
+        let dishesCount = 0;
+        let dishesArray = $scope.dishes.split(',');
 
-          if (nani) {
-              $scope.message += ' (' + n + ' empty dishe(s`) name(s) entered!)';
-          }
+        for(let i = 0; i < dishesArray.length; i++){
+            if (dishesArray[i] !== ""){
+                dishesCount ++;
+            }
+        }
+        if (dishesCount == 0){
+            $scope.messages = "Please enter data first";
+            
+        } else {
+            if(dishesCount <= 3){
+                $scope.messages = "Enjoy!";
+                
+            } else{
+                $scope.messages = "Too much!";
+                
+            }
+        }
+      };
+      
+  };
 
-      }
-      ;
-
-  }
+  
 
 }
 )();
